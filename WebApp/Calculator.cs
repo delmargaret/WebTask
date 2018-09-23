e48c4036af7b10068d1658936bdfb5438a9cbc30
delmargaret <ri.tysik@mail.ru>
2018-09-23 21:20:44 +0300

    создан отдельный класс для вычислений

---
+++ WebApp/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp
+{
+    public class Calculator
+    {
+        public double x1, x2, discr, a, b, c;
+        public string paramss;
+        public string res;
+
+        public void Count(double a, double b, double c)
+        {
+            paramss = "a = " + a.ToString() + " b = " + b.ToString() + " c = " + c.ToString();
+            discr = (b * b) - (4 * a * c);
+            if (discr < 0)
+            {
+                x1 = ((-b) / (2 * a));
+                x2 = (Math.Sqrt(Math.Abs(discr))) / (2 * a);
+                res = "x1 = " + Math.Round(x1, 2) + " + " + Math.Round(x2, 2) + "i" + " x2 = " + Math.Round(x1, 2) + " - " + Math.Round(x2, 2) + "i";
+            }
+            else if (discr == 0)
+            {
+                x1 = (-b) / (2 * a);
+                x2 = 0;
+                res = "x1 = " + Math.Round(x1, 2);
+            }
+            else
+            {
+                x1 = ((-b) + (Math.Sqrt(discr))) / (2 * a);
+                x2 = ((-b) - (Math.Sqrt(discr))) / (2 * a);
+                res = "x1 = " + Math.Round(x1, 2) + " x2 = " + Math.Round(x2, 2);
+            }
+
+        }
+    }
+}
--- WebApp/Handler.cs
+++ WebApp/Handler.cs
@@ -7,9 +7,6 @@ namespace WebApp
 {
     public class Handler : IHttpHandler
     {
-        public double x1, x2, discr, a, b, c;
-        string paramss;
-        string res;
 
         public bool IsReusable
         {
@@ -24,12 +21,14 @@ namespace WebApp
             NameValueCollection result;
             result = req.QueryString;
 
+            Calculator calc = new Calculator();
+
             int count = result.AllKeys.Length;
             if (count == 3)
             {
-                Count(Convert.ToInt32(result[result.AllKeys[0]]), Convert.ToInt32(result[result.AllKeys[1]]), Convert.ToInt32(result[result.AllKeys[2]]));
-                resp.Write("<p>" + paramss + "</p>");
-                resp.Write("<p>" + res + "</p>");
+                calc.Count(Convert.ToInt32(result[result.AllKeys[0]]), Convert.ToInt32(result[result.AllKeys[1]]), Convert.ToInt32(result[result.AllKeys[2]]));
+                resp.Write("<p>" + calc.paramss + "</p>");
+                resp.Write("<p>" + calc.res + "</p>");
 
             }
 
@@ -39,31 +38,6 @@ namespace WebApp
                 {
                     resp.Write("<p>" + "key: " + key + " value: " + result[key] + "</p>");
                 }
-            }
-
-        }
-
-        public void Count(double a, double b, double c)
-        {
-            paramss = "a = " + a.ToString() + " b = " + b.ToString() + " c = " + c.ToString();
-            discr = (b * b) - (4 * a * c);
-            if (discr < 0)
-            {
-                x1 = ((-b) / (2 * a));
-                x2 = (Math.Sqrt(Math.Abs(discr))) / (2 * a);
-                res = "x1 = " + Math.Round(x1,2) + " + " + Math.Round(x2, 2) + "i" + " x2 = " + Math.Round(x1, 2) + " - " + Math.Round(x2, 2) + "i";
-            }
-            else if (discr == 0)
-            {
-                x1 = (-b) / (2 * a);
-                x2 = 0;
-                res = "x1 = " + Math.Round(x1, 2);
-            }
-            else
-            {
-                x1 = ((-b) + (Math.Sqrt(discr))) / (2 * a);
-                x2 = ((-b) - (Math.Sqrt(discr))) / (2 * a);
-                res = "x1 = " + Math.Round(x1, 2) + " x2 = " + Math.Round(x2, 2);
             }
 
         }
