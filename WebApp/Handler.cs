a7e15bdc68ea13bfb76444fe048df3d9e4b32ce4
delmargaret <ri.tysik@mail.ru>
2018-09-23 21:31:09 +0300

    добавлено чтение html из файла и ввод параметров вида "?solve/2/3/1"

--- WebApp/Handler.cs
+++ WebApp/Handler.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Specialized;
 using System.IO;
+using System.Text;
 using System.Web;
 
 namespace WebApp
 {
     public class Handler : IHttpHandler
     {
+        double a, b, c;
+        string text;
 
         public bool IsReusable
         {
@@ -21,25 +24,55 @@ namespace WebApp
             NameValueCollection result;
             result = req.QueryString;
 
-            Calculator calc = new Calculator();
-
-            int count = result.AllKeys.Length;
-            if (count == 3)
+            ReadFile($"E:/WebTask/Web/WebApp/a.html");
+            string query = req.Url.Query.ToString();
+            if (query.Length == 0)
             {
-                calc.Count(Convert.ToInt32(result[result.AllKeys[0]]), Convert.ToInt32(result[result.AllKeys[1]]), Convert.ToInt32(result[result.AllKeys[2]]));
-                resp.Write("<p>" + calc.paramss + "</p>");
-                resp.Write("<p>" + calc.res + "</p>");
-
+                resp.Write("no parametrs");
             }
-
             else
             {
-                foreach (var key in result.AllKeys)
+                string[] abc = query.Split('/');
+                if (abc.Length >= 4)
                 {
-                    resp.Write("<p>" + "key: " + key + " value: " + result[key] + "</p>");
+                    a = Convert.ToInt32(abc[1]);
+                    b = Convert.ToInt32(abc[2]);
+                    c = Convert.ToInt32(abc[3]);
+
+                    StringBuilder sb = new StringBuilder();
+                    Calculator calc = new Calculator();
+                    calc.Count(a, b, c);
+                    if (calc.discr < 0)
+                    {
+                        string xx1 = string.Format("{0}+{1}i", Math.Round(calc.x1, 2).ToString(), Math.Round(calc.x2, 2).ToString());
+                        string xx2 = string.Format("{0}-{1}i", Math.Round(calc.x1, 2).ToString(), Math.Round(calc.x2, 2).ToString());
+                        sb.AppendFormat(text, a.ToString(), b.ToString(), c.ToString(), xx1, xx2);
+                    }
+                    else if (calc.discr == 0)
+                    {
+                        sb.AppendFormat(text, a.ToString(), b.ToString(), c.ToString(), Math.Round(calc.x1, 2).ToString(), "doesn't exist");
+                    }
+                    else
+                    {
+                        sb.AppendFormat(text, a.ToString(), b.ToString(), c.ToString(), Math.Round(calc.x1, 2).ToString(), Math.Round(calc.x2, 2).ToString());
+                    }
+                    resp.Write(sb);
                 }
+                else
+                {
+                    resp.Write("not enough parametrs");
+                }
+
             }
 
+        }
+
+        public void ReadFile(string filename)
+        {
+            using (StreamReader sr = new StreamReader(filename))
+            {
+                text = sr.ReadToEnd();
+            }
         }
     }
 }
